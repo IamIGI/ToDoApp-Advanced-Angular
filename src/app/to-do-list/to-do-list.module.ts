faebf312eaa7f82b25f8bb1a052dc4196f84a94c
i.kłusek <J6ZkqwVUn>
2023-03-30 15:20:11 +0200

    ToDoList

--- src/app/to-do-list/to-do-list.module.ts
+++ src/app/to-do-list/to-do-list.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { ToDoListRoutingModule } from './to-do-list-routing.module';
 import { ToDoListComponent } from './to-do-list.component';
 
 @NgModule({
   declarations: [ToDoListComponent],
-  imports: [RouterModule, ToDoListRoutingModule, CommonModule],
+  imports: [RouterModule, ToDoListRoutingModule, CommonModule, MatIconModule],
 })
 export class ToDoListModule {}
