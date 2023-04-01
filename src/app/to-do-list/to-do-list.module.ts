8f79321df3b4f67d5c198d2051f36a3f23498432
i.kłusek <J6ZkqwVUn>
2023-04-01 11:43:42 +0200

    Dynamic Progress Bar

--- src/app/to-do-list/to-do-list.module.ts
+++ src/app/to-do-list/to-do-list.module.ts
@@ -4,9 +4,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { ToDoListRoutingModule } from './to-do-list-routing.module';
 import { ToDoListComponent } from './to-do-list.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
-  declarations: [ToDoListComponent],
+  declarations: [ToDoListComponent, ProgressBarComponent],
   imports: [RouterModule, ToDoListRoutingModule, CommonModule, MatIconModule],
 })
 export class ToDoListModule {}
